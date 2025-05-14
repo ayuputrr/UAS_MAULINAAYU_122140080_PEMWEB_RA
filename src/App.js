0d21e93d79baf04cc386398af61b06e48955183e
maulinaayu_122140080 <150534522+ayuputrr@users.noreply.github.com>
2025-05-14 20:53:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-// src/App.js
-import { Routes, Route, Navigate } from "react-router-dom";
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import UserLayout from "./pages/user/UserLayout";
 import UserDashboard from "./pages/user/UserDashboard";
 import PinjamanPage from "./pages/user/PinjamanPage";
-import KartuPage from "./pages/user/KartuPage";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import Login from "./pages/Login";
 
-function App() {
+
+export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/user" />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/admin" element={<AdminDashboard />} />
-
       <Route path="/user" element={<UserLayout />}>
         <Route index element={<UserDashboard />} />
         <Route path="pinjaman" element={<PinjamanPage />} />
-        <Route path="kartu" element={<KartuPage />} />
+
       </Route>
     </Routes>
   );
 }
-
-export default App;
