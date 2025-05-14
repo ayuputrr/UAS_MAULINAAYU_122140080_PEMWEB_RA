a50b06520967b3073e52bd69dc97d59ef9b48029
maulinaayu_122140080 <150534522+ayuputrr@users.noreply.github.com>
2025-05-14 20:57:09 +0700

    Update UserLayout.js

--- src/pages/user/UserLayout.js
+++ src/pages/user/UserLayout.js
@@ -1,13 +1,11 @@
-// src/pages/user/UserLayout.js
+
 import { Outlet } from "react-router-dom";
-import BottomNav from "../../components/BottomNav";
+import BottomNav from "../../components/BottomNav"; 
 
 export default function UserLayout() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      <main className="flex-grow overflow-y-auto pb-24 px-4 pt-4">
-        <Outlet />
-      </main>
+    <div className="min-h-screen bg-gray-50 pb-20">
+      <Outlet /> 
       <BottomNav />
     </div>
   );
