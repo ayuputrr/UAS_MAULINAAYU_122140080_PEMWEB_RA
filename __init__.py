acdd884ed6dfadc4b7cab4cbe3115ed5b80f62ab
maulinaayu_122140080 <150534522+ayuputrr@users.noreply.github.com>
2025-05-29 21:40:03 +0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from pyramid.config import Configurator
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.include('.models')    # pastikan ini ada models/__init__.py dengan includeme()
-    config.include('.routes')    # routes.py untuk konfigurasi routing
-    config.scan()
-    return config.make_wsgi_app()
